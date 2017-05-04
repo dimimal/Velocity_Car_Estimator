e84653b3ad0cfee045d80a7ad1d08cdc66993797
dimimal <dimimallios@gmail.com>
2017-05-04 03:20:16 +0300

    Doppler attempt

--- main.m
+++ main.m
@@ -1,3 +1,14 @@
 % Author: Mallios Dimitris
 % 
 %
+%x = SpeedToDopplerShift(0);
+freq = 1e9;				%Radar frequency
+observer_speed = 10;
+car_speed = 0;
+lightspeed = 299792458; % Speed of light in m/s
+%v = 23.0;
+
+measured_freq = freq * (lightspeed + observer_speed)/(lightspeed + car_speed); 
+doppler_shift = freq - measured_freq 
+%lambda = lightspeed/freq;
+%dopplershift = speed2dop(v,lambda)
